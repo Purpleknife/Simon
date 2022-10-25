77e9da98e0a04fa0d7055374ce754c3dcc989465
Hind <hind.nayhi@gmail.com>
2022-10-24 18:19:29 -0600

    Make the buttons glowy when clicked.

--- front-end/src/components/Game.js
+++ front-end/src/components/Game.js
@@ -1,10 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import Intro from './Intro';
 
 import './Game.scss';
 
 const Game = () => {
+  const [redStyle, setRedStyle] = useState('red');
+  const [greenStyle, setGreenStyle] = useState('green');
+  const [blueStyle, setBlueStyle] = useState('blue');
+  const [yellowStyle, setYellowStyle] = useState('yellow');
+
+  const changeStyle = (condition) => {
+    if (condition === 'red') {
+      setRedStyle('red-neon');
+      setTimeout(() => {
+        setRedStyle('red');
+      }, "700");
+    }
+    if (condition === 'green') {
+      setGreenStyle('green-neon');
+      setTimeout(() => {
+        setGreenStyle('green');
+      }, "700");
+    }
+    if (condition === 'blue') {
+      setBlueStyle('blue-neon');
+      setTimeout(() => {
+        setBlueStyle('blue');
+      }, "700");
+    }
+    if (condition === 'yellow') {
+      setYellowStyle('yellow-neon');
+      setTimeout(() => {
+        setYellowStyle('yellow');
+      }, "700");
+    }
+  };
+
   return (
     <div className='container'>
       <div className='intro_game'>
@@ -14,9 +46,9 @@ const Game = () => {
       <div className='game'>
         <span id='level'>Level 1 / 20</span>
         <div id='time'><span>12:30</span></div><br />
-        <button className="red"></button>
+        <button className={redStyle} onClick={() => changeStyle('red')}></button>
         <div className='middle'>
-          <button className="green"></button>
+          <button className={greenStyle} onClick={() => changeStyle('green')}></button>
 
           <div className='mid'>
             <div id='count'>
@@ -26,9 +58,9 @@ const Game = () => {
             <button className="start">Start</button>
           </div>
 
-          <button className="blue"></button>
+          <button className={blueStyle} onClick={() => changeStyle('blue')}></button>
         </div>
-        <button className="yellow"></button>
+        <button className={yellowStyle} onClick={() => changeStyle('yellow')}></button>
       </div>
 
     </div>
