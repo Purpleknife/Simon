bf3b9683e729ee744dace51bdd4794c4ac77f0e1
Hind <hind.nayhi@gmail.com>
2022-10-25 16:43:49 -0600

    Add a function to compare the player's input and the game sequence.

--- front-end/src/helpers/helpers.js
+++ front-end/src/helpers/helpers.js
@@ -1,15 +1,30 @@
 
 /*
-Helper function to change the order in the array.
+shuffle: function to change the order in the array gameRandomOutput.
+eqArrays: function to compare the player's input and the game sequence.
 */
-
-const arr2 = ['red', 'green', 'blue', 'yellow'];
 
 
 export const shuffle = (arr2) => {
   return arr2.sort((a, b) => { 
-    return [Math.random() - 0.5]; 
+    return [Math.random() - 0.5];
   });
 };
 
-//console.log(shuffle(arr2));
+
+export const eqArrays = function(arr1, arr2) { //Compare both arrays and if they're a perfect match, return true.
+  if (arr1.length !== arr2.length) { //Compare length first, if it doesn't match, exit function and return false.
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++) { //Compare each element one by one if length matches.
+    if (arr1[i] !== arr2[i] && (!Array.isArray(arr1[i]) || !Array.isArray(arr2[i]))) {
+      return false;
+    }
+    if (Array.isArray(arr1[i]) && Array.isArray(arr2[i])) { // Added this condition (recursion) to handle nested arrays.
+      if (!eqArrays(arr1[i], arr2[i])) {
+        return false;
+      }
+    }
+  }
+  return true; //If length and all the elements are a perfect match, return true.
+};
